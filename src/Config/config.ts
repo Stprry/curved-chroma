7a47de033eca0761afd8ff1398de35066ffe96a6
Sam Perry <samperry.dev@gmail.com>
2022-11-12 23:18:37 +0000

    wip

---
+++ src/Config/config.ts
@@ -0,0 +1,10 @@
+type linkType = {
+	title: string;
+	href: string;
+};
+
+export const Links: linkType[] = [
+	{ title: "Home", href: "/" },
+	{ title: "about", href: "/about" },
+	{ title: "contact", href: "/contact" }
+];
